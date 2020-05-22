dbe0bfbe262af8e0dfc18df54fe30d5e9f7d951e
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2020-05-22 12:54:26 +0300

    feat: add swagger, enable cors, set global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1');
+  app.use(helmet());
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+  .setTitle('Authentication API')
+  .setDescription('The API description')
+  .setVersion('1.0')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
