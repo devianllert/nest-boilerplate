869f09e561801d3d64c602fc866419f05c71dc31
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2020-05-22 19:05:23 +0300

    feat: add mailer module

---
+++ src/modules/mailer/mailer.module.ts
@@ -0,0 +1,35 @@
+/* eslint-disable import/no-unresolved */
+import path from 'path';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MailModule, MustacheCompiler } from '@app/mail';
+
+import { MailerService } from './mailer.service';
+
+@Module({
+  imports: [
+    MailModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        transport: {
+          service: 'gmail',
+        },
+        defaults: {
+          from: config.get('mail.MAIL_FROM'),
+          secure: true,
+          auth: {
+            user: config.get('mail.MAIL_USER_NAME'),
+            pass: config.get('mail.MAIL_USER_PASS'),
+          },
+        },
+        template: {
+          dir: path.join(__dirname, '../templates'),
+          compiler: new MustacheCompiler(),
+        },
+      }),
+    }),
+  ],
+  providers: [MailerService],
+  exports: [MailerService],
+})
+export class MailerModule {}
---
+++ src/modules/mailer/mailer.service.ts
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-unresolved */
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { MailService } from '@app/mail';
+
+@Injectable()
+export class MailerService {
+  constructor(
+    private readonly mailService: MailService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async sendRegistrationMail(email: string, username: string, token: string) {
+    try {
+      const link = `${this.configService.get<string>('mail.CLIENT_URL')}${token}`;
+
+      await this.mailService.sendMail({
+        priority: 'high',
+        to: email,
+        subject: 'Registration',
+        template: 'email-verification',
+        context: {
+          username,
+          link,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
